ce803cbb4f5f0dbc453cb0df067e1b2741f68199
Leo Gomez <leoneljovel49@gmail.com>
2026-01-07 23:09:59 +0100

    Update skill animation selectors and add descriptive comment

--- JavaScript/SkillAnimationGSAP.js
+++ JavaScript/SkillAnimationGSAP.js
@@ -1,12 +1,12 @@
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector(".form-container");
+    const footer = document.querySelector(".footer-container");
     const lastCard = document.querySelector(".card.scroll");
     const pinnedSections = gsap.utils.toArray(".pinned");
 
     pinnedSections.forEach((section, index, sections) => {
-        let cards = section.querySelector(".skill-card");
+        let img = section.querySelector(".skill-content");
 
         let nextSection = sections[index + 1] || lastCard;
         //This endScalePoint defines the point where the scale animation will end.
@@ -17,13 +17,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 trigger: section,
                 start: "top top",
                 //This end will pin the section until the next one reaches the top of the viewport.
-                end: index === sections.length ? `+=${lastCard.offsetHeight / 2}` : form.offsetTop - window.innerHeight,
+                end: index === sections.length ? `+=${lastCard.offsetHeight / 2}` : footer.offsetTop - window.innerHeight,
                 pin: true,
                 pinSpacing: false,
-                scrub: 1,       
+                scrub: 1,
+                
             }
         })
-        gsap.fromTo(cards,{scale: 1}, {
+        //Image Scale Animation When Is Going Behind the Next Image
+        gsap.fromTo(img,{scale: 1}, {
             scale: .5,
             ease: "none",
             scrollTrigger:{
