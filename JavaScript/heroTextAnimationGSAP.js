194331dae44b6d9d2a707d2f0a1db0c19a419ac3
Leo Gomez <leoneljovel49@gmail.com>
2026-01-14 23:32:38 +0100

    Refactor hero text animation for improved readability and performance

--- JavaScript/heroTextAnimationGSAP.js
+++ JavaScript/heroTextAnimationGSAP.js
@@ -1,25 +1,31 @@
-//Main Text Animation
-gsap.registerPlugin(SplitText)
+export function initHeroTextAnimation() {
+    gsap.registerPlugin(SplitText)
 
-document.fonts.ready.then(() => {
-    gsap.set(".paragraph", { opacity: 1});
+    return new Promise((resolve) => {
+        document.fonts.ready.then(() => {
+            gsap.set(".paragraph", { opacity: 1, y: 0 });
 
-    let split;
-    SplitText.create(".paragraph", {
-        type: "words, lines",
-        wordsClass: "word",
-        linesClass: "line",
-        autoSplit: true,
-        mask: "words",
-        onSplit: (self) => {
-            split = gsap.from(self.words, {
-                y: 120,
-                duration: 1.2,
-                opacity: 0,
-                stagger: .09,
-                ease: "back.inOut",
+            let split;
+            SplitText.create(".paragraph", {
+                type: "words, lines",
+                wordsClass: "word",
+                linesClass: "line",
+                autoSplit: true,
+                mask: "words",
+                onSplit: (self) => {
+                    split = gsap.from(self.words, {
+                        y: 100,
+                        duration: 1,
+                        opacity: 0,
+                        stagger: .09,
+                        ease: "back.inOut",
+                        onComplete: () => {
+                            resolve()
+                        }
+                    })
+                    return split;
+                }
             })
-            return split;
-        }
+        })
     })
-})
+}
