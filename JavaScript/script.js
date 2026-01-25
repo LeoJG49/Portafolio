9e7cc158d5bd220b0a7dffc4a84e9ca9d1cc0bab
Leo Dev <leoneljovel49@gmail.com>
2026-01-25 20:42:44 +0100

    Reorganize animation initialization sequence and streamline smooth scroll functionality

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,41 +5,26 @@ import './SkillAnimationGSAP.js'
 import './SkillTextAnimationGSAP.js'
 import './cardsAnimationGSAP.js'
 import { initPageLoader } from './pageLoadingAnimationGSAP.js'
-import { initHeroAnimations } from './gsapAnimations.js'
 import { initHeroTextAnimation } from './heroTextAnimationGSAP.js'
+import { initHeroAnimations } from './gsapAnimations.js'
 
 //TODO las animaciones del header no se realizan
 // Además que la animación del texto del hero se ve antes que termine la animación 
 // del preloader. Arreglar el orden de las animaciones
 
+async function init() {
+    await initPageLoader ()
+    initHeroAnimations()
+    initHeroTextAnimation()
+}
+
+init()
+
 //Smooth Scroll
-function smoothScroll () {
-    const lenis = new Lenis()
-    
-    lenis.on('scroll', () => {})
-    
-    function raf(time) {
-        lenis.raf(time)
-        requestAnimationFrame(raf)
-    }
-    
+const lenis = new Lenis()
+lenis.on('scroll', () => {})
+function raf(time) {
+    lenis.raf(time)
     requestAnimationFrame(raf)
 }
-
-// Inicializar animaciones en secuencia
-async function initAnimations() {
-    // 1. Ejecutar preloader primero
-    await initPageLoader()
-    
-    // 2. Luego ejecutar animaciones del hero
-    initHeroAnimations()
-    
-    // 3. Finalmente ejecutar animación del texto del hero
-    await initHeroTextAnimation()
-}
-
-// Esperar a que el DOM esté listo
-document.addEventListener('DOMContentLoaded', () => {
-    initAnimations()
-    smoothScroll()
-})
+requestAnimationFrame(raf)
