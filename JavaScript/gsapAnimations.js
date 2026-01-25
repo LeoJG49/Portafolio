b09187d9dd4e7bedb8672234053320ec475418b8
Leo Dev <leoneljovel49@gmail.com>
2026-01-25 20:42:30 +0100

    Refactor header animation easing and remove unused logo and menu animations

--- JavaScript/gsapAnimations.js
+++ JavaScript/gsapAnimations.js
@@ -3,23 +3,6 @@ export function initHeroAnimations() {
     gsap.to("header", {
         y: 0,
         duration: 1,
-        ease: "ease.out",
-    })
-
-    //Logo animation
-    gsap.to(".head-link", {
-        y: 0,
-        duration: 1,
-        delay: .3,
-        ease: "bounce",
-    })
-
-    //Menu nav animation
-    gsap.to(".objects", {
-        y: 0,
-        duration: 1,
-        delay: .3,
-        ease: "bounce",
-        stagger: .25,
+        ease: "power3.inOut",
     })
 }
