51814fdbcb34f54eab55ae3d903aec09571d1981
Leo Dev <leoneljovel49@gmail.com>
2026-01-26 14:28:34 +0100

    Add comments to explain scroll behavior in dynamic header

--- JavaScript/dinamicHeader.js
+++ JavaScript/dinamicHeader.js
@@ -1,9 +1,9 @@
 const header = document.getElementById('dinamic-header')
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 50) {
+    if(window.scrollY > 50) { //When scroll is more than 50px assing the class
         header.classList.add('shrink');
     } else {
-        header.classList.remove('shrink');
+        header.classList.remove('shrink'); 
     }
 })
