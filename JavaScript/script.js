767a24a60cd1c136b25aa88389bb7b8c45b7dfca
Leo Dev <leoneljovel49@gmail.com>
2026-01-26 14:29:07 +0100

    refactor: clean up imports and remove unused code in script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,23 +1,17 @@
 import './dinamicHeader.js'
-import './fadeInContainer.js'
 import './footerTextHoverAnimation.js'
 import './SkillAnimationGSAP.js'
 import './SkillTextAnimationGSAP.js'
 import './cardsAnimationGSAP.js'
 import { initPageLoader } from './pageLoadingAnimationGSAP.js'
 import { initHeroTextAnimation } from './heroTextAnimationGSAP.js'
-import { initHeroAnimations } from './gsapAnimations.js'
-
-//TODO las animaciones del header no se realizan
-// Además que la animación del texto del hero se ve antes que termine la animación 
-// del preloader. Arreglar el orden de las animaciones
+import { initHeroAnimations } from './headerAnimation.js'
 
 async function init() {
     await initPageLoader ()
     initHeroAnimations()
     initHeroTextAnimation()
 }
-
 init()
 
 //Smooth Scroll
