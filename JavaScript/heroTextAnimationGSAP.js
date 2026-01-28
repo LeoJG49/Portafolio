0e9c1fd2e755040288a6f923d05d5f675e9db5c6
Leo Dev <leoneljovel49@gmail.com>
2026-01-28 19:09:30 +0100

    Refactor hero text animation function to remove promise resolution and streamline code structure

--- JavaScript/heroTextAnimationGSAP.js
+++ JavaScript/heroTextAnimationGSAP.js
@@ -1,30 +1,25 @@
 export function initHeroTextAnimation() {
-    return new Promise((resolve) => {
-        document.fonts.ready.then(() => {
-            gsap.set(".paragraph", { opacity: 1, y: 0 });
-
-            let split;
-            SplitText.create(".paragraph", {
-                type: "words, lines",
-                wordsClass: "word",
-                linesClass: "line",
-                autoSplit: true,
-                mask: "words",
-                onSplit: (self) => {
-                    split = gsap.from(self.words, {
-                        y: 100,
-                        duration: 1,
-                        opacity: 0,
-                        stagger: .09,
-                        ease: "back.inOut",
-                        onComplete: () => {
-                            resolve()
-                        }
-                    })
-                    ScrollTrigger.refresh();
-                    return split;
-                }
-            })
+    gsap.registerPlugin(SplitText);
+    document.fonts.ready.then(() => {
+        gsap.set(".paragraph", { opacity: 1, y: 0 });
+        let split;
+        SplitText.create(".paragraph", {
+            type: "words, lines",
+            wordsClass: "word",
+            linesClass: "line",
+            autoSplit: true,
+            mask: "words",
+            onSplit: (self) => {
+                split = gsap.from(self.words, {
+                    y: 100,
+                    duration: 1,
+                    opacity: 0,
+                    stagger: .09,
+                    ease: "back.inOut",
+                })
+                ScrollTrigger.refresh();
+                return split;
+            }
         })
     })
 }
